d6a5ad927866ac3387ead70a506811c8d01e6cdd
sunsai <1842574286@qq.com>
2022-07-08 17:36:55 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import Button from './components/Button'
-import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button/>
-    </div>
+    <>
+        app...
+    </>
   );
 }
-
-export default App;
